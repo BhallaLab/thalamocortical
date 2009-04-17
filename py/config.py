9d168de224e3fa6333d8f38c022993ffc9d5de49
Subhasis Ray <ray.subhasis@gmail.com>
2009-04-18 01:06:56 +0530

    New version of the channels - skipped Ca2+ depenedent K+ channels, some variations of Na+ channels.

---
+++ py/config.py
@@ -0,0 +1,63 @@
+# config.py --- 
+# 
+# Filename: config.py
+# Description: 
+# Author: subhasis ray
+# Maintainer: 
+# Created: Fri Apr 17 14:36:30 2009 (+0530)
+# Version: 
+# Last-Updated: Fri Apr 17 14:43:35 2009 (+0530)
+#           By: subhasis ray
+#     Update #: 5
+# URL: 
+# Keywords: 
+# Compatibility: 
+# 
+# 
+
+# Commentary: 
+# 
+# This contains global variables used in other modules.
+# 
+# 
+
+# Change log:
+# 
+# 
+# 
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+# 
+# 
+
+# Code:
+
+import sys
+sys.path.append('/home/src/sim/cortical/py')
+import moose
+context = moose.PyMooseBase.getContext()
+simdt = 1e-4
+plotdt = 1e-4
+
+vmin = 120e-3
+vmax = 40e-3
+ndivs = 640
+dv = (vmax - vmin) / ndivs
+
+
+
+# 
+# config.py ends here
