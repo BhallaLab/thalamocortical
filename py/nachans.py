c0a88d79f374c9129bd301a03eeecce6070530c9
Subhasis Ray <ray.subhasis@gmail.com>
2009-04-20 23:29:45 +0530

    NaF_TCR and KDR working together - matching NEURON.

--- py/nachans.py
+++ py/nachans.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Fri Apr 17 23:58:13 2009 (+0530)
 # Version: 
-# Last-Updated: Mon Apr 20 22:44:35 2009 (+0530)
+# Last-Updated: Mon Apr 20 23:25:44 2009 (+0530)
 #           By: subhasis ray
-#     Update #: 23
+#     Update #: 56
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -115,7 +115,7 @@ class NaP(ChannelBase):
         self.xGate.B.dumpFile("nap_xb.plot")
 
 
-class NaP_f(ChannelBase):
+class NaPF(ChannelBase):
     """Persistent Na+ current, fast"""
     def __init__(self, name, parent):
         ChannelBase.__init__(self, name, parent, 3)
@@ -129,6 +129,53 @@ class NaP_f(ChannelBase):
             self.xGate.A[i] = tau_m[i]
             self.xGate.B[i] = m_inf[i]
         self.xGate.tweakTau()
+
+class NaPF_TCR(ChannelBase):
+    """Persistent Na+ channel specific to TCR cells. Only difference
+    with NaPF is power of m is 1 as opposed 3."""
+    def __init__(self, name, parent):
+        ChannelBase.__init__(self, name, parent, 1)
+        self.Ek = 50e-3
+        v = linspace(config.vmin, config.vmax, config.ndivs + 1)
+        tau_m = where(v < -30e-3, \
+                           1.0e-3 * (0.025 + 0.14 * exp((v  + 30.0e-3) / 10.0e-3)), \
+                           1.0e-3 * (0.02 + 0.145 * exp((- v - 30.0e-3) / 10.0e-3)))
+        m_inf = 1.0 / (1.0 + exp((-v - 38e-3) / 10e-3))
+        for i in range(config.ndivs + 1):
+            self.xGate.A[i] = tau_m[i]
+            self.xGate.B[i] = m_inf[i]
+        self.xGate.tweakTau()
+
+class NaF_TCR(ChannelBase):
+    """Fast Na+ channel for TCR cells. This is almost identical to
+    NaF, but there is a nasty voltage shift in the tables."""
+    def __init__(self, name, parent):
+        ChannelBase.__init__(self, name, parent, 3, 1)
+        self.Ek = 50e-3
+        shift_mnaf = -5.5e-3
+        shift_hnaf = -7e-3
+        v = linspace(config.vmin, config.vmax, config.ndivs + 1) 
+        tau_h = 1.0e-3 * (0.15 + 1.15 / ( 1.0 + exp(( v + 37.0e-3) / 15.0e-3)))        
+        v = v + shift_hnaf
+        h_inf = 1.0 / (1.0 + exp((v + 62.9e-3) / 10.7e-3))
+        v = v - shift_hnaf + shift_mnaf
+        tau_m = where(v < -30e-3, \
+                          1.0e-3 * (0.025 + 0.14 * exp((v + 30.0e-3) / 10.0e-3)), \
+                          1.0e-3 * (0.02 + 0.145 * exp(( - v - 30.0e-3) / 10.0e-3)))
+        m_inf = 1.0 / (1.0 + exp(( - v - 38e-3) / 10e-3))
+
+        for i in range(config.ndivs + 1):
+            self.xGate.A[i] = tau_m[i]
+            self.xGate.B[i] = m_inf[i]
+            self.yGate.A[i] = tau_h[i]
+            self.yGate.B[i] = h_inf[i]
+        self.xGate.tweakTau()
+        self.yGate.tweakTau()
+        self.xGate.A.dumpFile("naf_xa.plot")
+        self.xGate.B.dumpFile("naf_xb.plot")
+        self.yGate.A.dumpFile("naf_ya.plot")
+        self.yGate.B.dumpFile("naf_yb.plot")
+        
 
 # 
 # nachans.py ends here
