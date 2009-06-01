edb892b831e489f47b266728110ebcf5f04092c8
Subhasis Ray <ray.subhasis@gmail.com>
2009-06-01 22:25:04 +0530

    Corrected the shameful mistake of forgetting to divide by 4 when using diameter for calculating cross section.

--- py/archan.py
+++ py/archan.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Mon Apr 27 15:34:07 2009 (+0530)
 # Version: 
-# Last-Updated: Sun May  3 23:30:18 2009 (+0530)
+# Last-Updated: Mon Jun  1 16:34:42 2009 (+0530)
 #           By: subhasis ray
-#     Update #: 4
+#     Update #: 5
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -26,20 +26,6 @@
 # 
 # 
 # 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 3, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; see the file COPYING.  If not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-# Floor, Boston, MA 02110-1301, USA.
 # 
 # 
 
