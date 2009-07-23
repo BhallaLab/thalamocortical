d75b09dc75a2ef670c2e864e14d926971c48a228
Subhasis Ray <ray.subhasis@gmail.com>
2009-07-23 23:49:47 +0530

    Updated compare.py to do a csv based comparison.

--- compare.py
+++ compare.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Fri Jul 17 18:01:06 2009 (+0530)
 # Version: 
-# Last-Updated: Tue Jul 21 15:03:35 2009 (+0530)
+# Last-Updated: Thu Jul 23 23:38:46 2009 (+0530)
 #           By: subhasis ray
-#     Update #: 69
+#     Update #: 116
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -57,7 +57,53 @@ def almost_equal(left, right, epsilon=1e-6):
         return ( 1 - left / right) < epsilon
 #!almost_equal
 
-if __name__ == '__main__':
+def comparecsv(left, right, epsilon=1e3, row_header=True, col_header=True):
+    ret = True
+    left_file = open(left, 'rb')
+    right_file = open(right, 'rb')
+    left_reader = csv.reader(left_file, delimiter=',')
+    right_reader = csv.reader(right_file, delimiter=',')
+    index = 0
+    if col_header:
+        left_header = left_reader.next()
+        right_header = right_reader.next()
+        index = 1
+
+    while True:
+        left_row = left_reader.next()
+        right_row = right_reader.next()
+        if left_row is None and not (right_row is None):
+            print left, 'run out of line after', index, 'rows'
+            ret = False
+            break
+        if right_row is None and not (left_row is None):
+            print right, 'run out of line after', index, 'rows'
+            ret = False
+            break
+        if len(left_row) != len(right_row):
+            print 'No. of columns differ: left - ', len(left_row), 'right -', len(right_row)
+            ret = False
+            break
+        start_col = 0
+        if row_header:
+            start_col = 1
+        col_no = start_col
+        for left_col, right_col, in zip(left_row[start_col:], right_row[start_col:]):
+            if not almost_equal(float(left_col), float(right_col), epsilon):
+                if not row_header:
+                    row = str(index)
+                else:
+                    row = str(left_row[0])
+                if not col_header:
+                    col = str(col_no)
+                else:
+                    col = str(left_header[col_no])
+                print 'Mismatch in row:%s, column:%s. Values: %f <> %f' % (row, col, left_col, right_col)
+                ret = False
+        return ret
+
+
+def compare_tcr():
     moose_file = open('py/tcr.txt', 'r')
     line = moose_file.readline()
     moose_header = line.split(',')
@@ -74,5 +120,7 @@ if __name__ == '__main__':
                 all_matching = False
     if all_matching: print 'All matching ...'
 
+if __name__ == "__main__":
+    print comparecsv('py/tcr.txt', 'nrn/TCR')
 # 
 # compare.py ends here
