7399f8320548cb30b91d3ce353939e4c4703b1c7
Subhasis Ray <ray.subhasis@gmail.com>
2009-09-17 00:41:00 +0530

    KC and CaL working fine with 0.3mA depolarizing current injection.

--- py/nachans.py
+++ py/nachans.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Fri Apr 17 23:58:13 2009 (+0530)
 # Version: 
-# Last-Updated: Wed Sep  9 15:11:15 2009 (+0530)
+# Last-Updated: Wed Sep 16 16:33:59 2009 (+0530)
 #           By: subhasis ray
-#     Update #: 122
+#     Update #: 133
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -52,13 +52,13 @@ from channel import ChannelBase
 
 class NaChannel(ChannelBase):
     """Dummy base class for all Na+ channels"""
-    def __init__(self, name, parent, x, y=0, Ek=50e-3):
-        ChannelBase.__init__(self, name, parent, x, y)
+    def __init__(self, name, parent, xpower, ypower=0.0, Ek=50e-3):
+        ChannelBase.__init__(self, name, parent, xpower=xpower, ypower=ypower)
         self.Ek = Ek
 
 class NaF(NaChannel):
     def __init__(self, name, parent, shift=-3.5e-3, Ek=50e-3):
-        NaChannel.__init__(self, name, parent, 3, 1, Ek)
+        NaChannel.__init__(self, name, parent, xpower=3.0, ypower=1.0, Ek=Ek)
         v = linspace(config.vmin, config.vmax, config.ndivs + 1) + shift
         tau_m = where(v < -30e-3, \
                           1.0e-3 * (0.025 + 0.14 * exp((v + 30.0e-3) / 10.0e-3)), \
@@ -74,10 +74,11 @@ class NaF(NaChannel):
             self.yGate.B[i] = h_inf[i]
         self.xGate.tweakTau()
         self.yGate.tweakTau()
+        self.X = 0.0
         
 class NaF2(NaChannel):
     def __init__(self, name, parent, shift=0.0, Ek=50e-3):
-        NaChannel.__init__(self, name, parent, 3, 1, Ek=Ek)
+        NaChannel.__init__(self, name, parent, xpower=3.0, ypower=1.0, Ek=Ek)
         print 'NaF2: shift =', shift
         v = linspace(config.vmin, config.vmax, config.ndivs + 1)
         tau_h = 1e-3 * (0.225 + 1.125 / ( 1 + exp( (  v + 37e-3 ) / 15e-3 ) ))
@@ -104,7 +105,7 @@ class NaF2(NaChannel):
 
 class NaP(NaChannel):
     def __init__(self, name, parent, Ek=50e-3):
-        NaChannel.__init__(self, name, parent, 1, Ek=Ek)
+        NaChannel.__init__(self, name, parent, xpower=1.0, Ek=Ek)
         v = linspace(config.vmin, config.vmax, config.ndivs + 1)
         tau_m = where(v < -40e-3, \
                           1.0e-3 * (0.025 + 0.14 * exp((v + 40e-3) / 10e-3)), \
@@ -119,7 +120,7 @@ class NaP(NaChannel):
 class NaPF(NaChannel):
     """Persistent Na+ current, fast"""
     def __init__(self, name, parent, Ek=50e-3):
-        NaChannel.__init__(self, name, parent, 3, Ek=Ek)
+        NaChannel.__init__(self, name, parent, xpower=3.0, Ek=Ek)
         v = linspace(config.vmin, config.vmax, config.ndivs + 1)
         tau_m = where(v < -30e-3, \
                            1.0e-3 * (0.025 + 0.14 * exp((v  + 30.0e-3) / 10.0e-3)), \
@@ -132,7 +133,7 @@ class NaPF(NaChannel):
 
 class NaPF_SS(NaChannel):
     def __init__(self, name, parent, shift=-2.5e-3, Ek=50e-3):
-        NaChannel.__init__(self, name, parent, 3, Ek=Ek)
+        NaChannel.__init__(self, name, parent, xpower=3.0, Ek=Ek)
         if shift is None:
             shift = -2.5e-3
         print 'NaPF_SS: shift =', shift
@@ -150,7 +151,7 @@ class NaPF_TCR(NaChannel):
     """Persistent Na+ channel specific to TCR cells. Only difference
     with NaPF is power of m is 1 as opposed 3."""
     def __init__(self, name, parent, shift=7e-3, Ek=50e-3):
-        NaChannel.__init__(self, name, parent, 1, Ek=Ek)
+        NaChannel.__init__(self, name, parent, xpower=1.0, Ek=Ek)
         v = linspace(config.vmin, config.vmax, config.ndivs + 1) + shift
         tau_m = where(v < -30e-3, \
                            1.0e-3 * (0.025 + 0.14 * exp((v  + 30.0e-3) / 10.0e-3)), \
@@ -165,7 +166,7 @@ class NaF_TCR(NaChannel):
     """Fast Na+ channel for TCR cells. This is almost identical to
     NaF, but there is a nasty voltage shift in the tables."""
     def __init__(self, name, parent, Ek=50e-3):
-        NaChannel.__init__(self, name, parent, 3, 1, Ek=Ek)
+        NaChannel.__init__(self, name, parent, xpower=3.0, ypower=1.0, Ek=Ek)
         shift_mnaf = -5.5e-3
         shift_hnaf = -7e-3
         v = linspace(config.vmin, config.vmax, config.ndivs + 1) 
