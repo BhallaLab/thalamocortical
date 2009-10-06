6c50140f0a3fbeac7201d04985b542c9e7ac9e35
Subhasis Ray <ray.subhasis@gmail.com>
2009-10-06 16:56:39 +0530

    Started work on Superficial Basket cells and a rework of the spiny stellate cells in the using .p files.

---
+++ py/supbasket.py
@@ -0,0 +1,52 @@
+# supbasket.py --- 
+# 
+# Filename: supbasket.py
+# Description: Superficial Layer 2/3 basket cells
+# Author: subhasis ray
+# Maintainer: 
+# Created: Tue Oct  6 16:52:28 2009 (+0530)
+# Version: 
+# Last-Updated: Tue Oct  6 16:52:40 2009 (+0530)
+#           By: subhasis ray
+#     Update #: 1
+# URL: 
+# Keywords: 
+# Compatibility: 
+# 
+# 
+
+# Commentary: 
+# 
+# 
+# 
+# 
+
+# Change log:
+# 
+# 
+# 
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+# 
+# 
+
+# Code:
+
+
+
+
+# 
+# supbasket.py ends here
