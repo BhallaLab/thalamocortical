be27ca43542a113fc2bf5f41603f755ed8273d8d
Subhasis Ray <ray.subhasis@gmail.com>
2010-10-11 18:23:30 +0530

    Added another version of the network: now empty. See notes for details.

---
+++ py/trbnet.py
@@ -0,0 +1,63 @@
+# trbnet.py --- 
+# 
+# Filename: trbnet.py
+# Description: 
+# Author: Subhasis Ray
+# Maintainer: 
+# Created: Mon Oct 11 17:52:29 2010 (+0530)
+# Version: 
+# Last-Updated: Mon Oct 11 17:56:50 2010 (+0530)
+#           By: Subhasis Ray
+#     Update #: 6
+# URL: 
+# Keywords: 
+# Compatibility: 
+# 
+# 
+
+# Commentary: 
+# 
+# This is for putting together the lessons learnt from experiments
+# with Graph as data structure to represent the netwrok.
+# 
+# I note the following points: 
+#
+# 1. It is nice to have the defining data-structure as a graph.  Each
+#    node represents a homogeneous population. The node attributes can
+#    store population specific information.
+#
+#    The connectivity information can be stored in the edges. The edge
+#    attributes represent the information on the synapses. Edge weight
+#    can represent pre-post ratio (no. of presynaptic cell of type
+#    source vertex connecting to each cell of type destination vertex).
+#
+
+# Change log:
+# 
+# 
+# 
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+# 
+# 
+
+# Code:
+
+
+
+
+# 
+# trbnet.py ends here
