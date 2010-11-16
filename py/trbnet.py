8260730b0c22376a977780217bea5bda3bc4911d
Subhasis Ray <ray.subhasis@gmail.com>
2010-11-17 02:16:37 +0530

    Fixed the postsynaptic compartment=None issue. It was happening because the loop traversed all cell-pairs - even when there were no connections.

--- py/trbnet.py
+++ py/trbnet.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Mon Oct 11 17:52:29 2010 (+0530)
 # Version: 
-# Last-Updated: Wed Nov 17 00:02:05 2010 (+0530)
-#           By: subha
-#     Update #: 751
+# Last-Updated: Wed Nov 17 02:12:03 2010 (+0530)
+#           By: Subhasis Ray
+#     Update #: 780
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -207,8 +207,8 @@ class TraubNet(object):
         tn = TraubFullNetData()
         self.celltype_graph = ig.Graph(0, directed=True)
         self.celltype_graph.add_vertices(len(tn.celltype))
-        self.nRT_TCR_ggaba_low = tn.nRT_g_gaba_low
-        self.nRT_TCR_ggaba_high = tn.nRT_g_gaba_high
+        self.nRT_TCR_ggaba_low = tn.nRT_TCR_ggaba_low
+        self.nRT_TCR_ggaba_high = tn.nRT_TCR_ggaba_high
         edge_count = 0
         start_index = 0
         for celltype in self.celltype_graph.vs:
@@ -220,23 +220,23 @@ class TraubNet(object):
                 pre_post_ratio = tn.pre_post_ratio[celltype.index][posttype.index]
                 if pre_post_ratio > 0:
                     self.celltype_graph.add_edges((celltype.index, posttype.index))
-                    self.celltype_graph.es[edge_count]['weight'] = 1.0 * pre_post_ratio / celltype['count']
-                    self.celltype_graph.es[edge_count]['gampa'] = tn.g_ampa_baseline[celltype.index][posttype.index]
-                    self.celltype_graph.es[edge_count]['gnmda'] = tn.g_nmda_baseline[celltype.index][posttype.index]
-                    self.celltype_graph.es[edge_count]['tauampa'] = tn.tau_ampa[celltype.index][posttype.index]
-                    self.celltype_graph.es[edge_count]['taunmda'] = tn.tau_nmda[celltype.index][posttype.index]
-                    self.celltype_graph.es[edge_count]['taugaba'] = tn.tau_gaba[celltype.index][posttype.index]
-                    self.celltype_graph.es[edge_count]['pscomps'] = str(tn.allowed_comps[celltype.index][posttype.index])
-                    self.celltype_graph.es[edge_count]['ekgaba'] = tn.ek_gaba[posttype.index]
-                    self.celltype_graph.es[edge_count]['ggaba'] = tn.g_gaba_baseline[celltype.index][posttype.index]
+                    new_edge = self.celltype_graph.es[edge_count]
+                    new_edge['weight'] = 1.0 * pre_post_ratio / celltype['count']
+                    new_edge['gampa'] = tn.g_ampa_baseline[celltype.index][posttype.index]
+                    new_edge['gnmda'] = tn.g_nmda_baseline[celltype.index][posttype.index]
+                    new_edge['tauampa'] = tn.tau_ampa[celltype.index][posttype.index]
+                    new_edge['taunmda'] = tn.tau_nmda[celltype.index][posttype.index]
+                    new_edge['taugaba'] = tn.tau_gaba[celltype.index][posttype.index]
+                    new_edge['pscomps'] = str(tn.allowed_comps[celltype.index][posttype.index])
+                    new_edge['ekgaba'] = tn.ek_gaba[posttype.index]
+                    new_edge['ggaba'] = tn.g_gaba_baseline[celltype.index][posttype.index]
                     if celltype['label'] == 'nRT':
                         if posttype['label'] == 'TCR':
-                            self.celltype_graph.es[edge_count]['taugabaslow'] = tn.nRT_TCR_tau_gaba_slow
-                            self.celltype_graph.es[edge_count]['ggaba'] = 'uniform %f %f' % (tn.nRT_g_gaba_low, tn.nRT_g_gaba_high) # How to specify distribution?
+                            new_edge['taugabaslow'] = tn.nRT_TCR_tau_gaba_slow
+                            new_edge['ggaba'] = 'uniform %g %g' % (tn.nRT_TCR_ggaba_low, tn.nRT_TCR_ggaba_high) # How to specify distribution?
                         elif posttype['label'] == 'nRT':
-                            self.celltype_graph.es[edge_count]['taugabaslow'] = tn.nRT_nRT_tau_gaba_slow
+                            new_edge['taugabaslow'] = tn.nRT_nRT_tau_gaba_slow
                     
-                    print celltype['label'], posttype['label'], self.celltype_graph.es[edge_count]['ggaba']
                     edge_count += 1
 
     def _read_celltype_graph(self):
@@ -268,6 +268,7 @@ class TraubNet(object):
             precount = int(pretype['count'])
             postcount = int(posttype['count'])
             connprob = float(edge['weight'])
+            
             ps_comps = numpy.array(eval(edge['pscomps']))
             config.LOGGER.debug('Connecting populations: pre=%s[:%d], post=%s[:%d], probability=%g' % (pretype['label'], pretype['count'], posttype['label'], posttype['count'], connprob))
             config.LOGGER.debug('ggaba= %s, type:%s' % (str(edge['ggaba']), edge['ggaba'].__class__.__name__))
@@ -287,6 +288,7 @@ class TraubNet(object):
                                     for postindex in range(postcount)
                                     for preindex in pre_indices[postindex]],
                                    dtype='int32')
+            config.LOGGER.debug(edge['pscomps'])
             self.ps_comp_mat.put(ps_comps[comp_indices.flatten()], syn_list[:,0], syn_list[:, 1])
             self.g_ampa_mat.put(float(edge['gampa']),
                                 syn_list[:, 0], syn_list[:,1])
@@ -328,16 +330,17 @@ class TraubNet(object):
                 precomp = precell.comp[precell.presyn]
                 for post_index in range(poststart, poststart+postcount):
                     postcell = self.index_cell_map[post_index]
-                    postcomp = postcell.comp[self.ps_comp_mat[pre_index, post_index]]
-                    print precomp, postcomp
-                    print precomp.path, postcomp.path
-                    print isinstance(precomp, moose.PyMooseBase), isinstance(postcomp, moose.PyMooseBase)
+                    postcompindex = int(self.ps_comp_mat[pre_index, post_index])
+                    postcomp = postcell.comp[postcompindex]
+                    if postcomp is None:
+                        continue
+                                            
                     g_ampa = self.g_ampa_mat[pre_index, post_index]
-                    if g_ampa != 0.0:
+                    if g_ampa != 0.0:                        
                         precomp.makeSynapse(postcomp, name='ampa', Ek=0.0, Gbar=g_ampa, tau1=syn_edge['tauampa'], tau2=0.0)
                     g_nmda = self.g_nmda_mat[pre_index, post_index]
                     if g_nmda != 0.0:
-                        precomp.makeSynapse(postcomp, name='nmda', className='NMDAChan', Ek=0.0, tau1=syn_edge['taunmda'], tau2=5e-3)
+                        precomp.makeSynapse(postcomp, name='nmda', classname='NMDAChan', Ek=0.0, tau1=syn_edge['taunmda'], tau2=5e-3)
                     g_gaba = self.g_gaba_mat[pre_index, post_index]
                     if g_gaba != 0.0:
                         if syn_edge['taugabaslow'] > 0.0:
