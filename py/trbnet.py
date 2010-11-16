7636f712372fdb6ee1b8735c570ca1f77b48ffae
Subhasis Ray <ray.subhasis@gmail.com>
2010-11-16 17:54:43 +0530

    Finished create_network funcion.

--- py/trbnet.py
+++ py/trbnet.py
@@ -6,9 +6,9 @@
 # Maintainer: 
 # Created: Mon Oct 11 17:52:29 2010 (+0530)
 # Version: 
-# Last-Updated: Tue Nov 16 16:11:07 2010 (+0530)
+# Last-Updated: Tue Nov 16 17:54:24 2010 (+0530)
 #           By: Subhasis Ray
-#     Update #: 729
+#     Update #: 743
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -306,6 +306,7 @@ class TraubNet(object):
 
     def create_network(self):
         """Instantiate the network in MOOSE"""
+        starttime = datetime.now()
         total_count = 0
         for celltype in self.celltype_graph.vs:
             cell_count = int(celltype['count'])
@@ -330,16 +331,19 @@ class TraubNet(object):
                     postcomp = postcell.comp[self.ps_comp_mat[pre_index, post_index]]
                     g_ampa = self.g_ampa_mat[pre_index, post_index]
                     if g_ampa != 0.0:
-                        precomp.makeSynapse(postcomp, name='ampa', Ek=0.0, Gbar=self.g_ampa, tau1=0.0, tau2=syn_edge['tauampa'])
+                        precomp.makeSynapse(postcomp, name='ampa', Ek=0.0, Gbar=self.g_ampa, tau1=syn_edge['tauampa'], tau2=0.0)
                     g_nmda = self.g_nmda_mat[pre_index, post_index]
                     if g_nmda != 0.0:
-                        precomp.makeSynapse(postcomp, name='nmda', Ek=0.0, tau1=syn_edge['taunmda'], tau2=5e-3)
+                        precomp.makeSynapse(postcomp, name='nmda', className='NMDAChan', Ek=0.0, tau1=syn_edge['taunmda'], tau2=5e-3)
                     g_gaba = self.g_gaba_mat[pre_index, post_index]
                     if g_gaba != 0.0:
-                        if syn_edge['taugabaslow'] > 0:
-                            precomp.makeSynapse(postcomp, name='gaba_slow', Ek=syn_edge['ekgaba'], tau1=syn_edge['taugabaslow'])
-                        precomp.makeSynapse(postcomp, name='gaba', Ek=syn_edge['ekgaba'], tau1=syn_edge['taugaba'])
-            
+                        if syn_edge['taugabaslow'] > 0.0:
+                            precomp.makeSynapse(postcomp, name='gaba_slow', Ek=syn_edge['ekgaba'], tau1=syn_edge['taugabaslow'], tau2=0.0)
+                        precomp.makeSynapse(postcomp, name='gaba', Ek=syn_edge['ekgaba'], tau1=syn_edge['taugaba'], tau2=0.0)
+        endtime = datetime.now()
+        delta = endtime - starttime
+        config.BENCHMARK_LOGGER.info('Finished network creation in: %g s' % (delta.days * 86400 + delta.seconds + 1e-6 * delta.microseconds))
+        
     def scale_populations(self, scale):
         """Scale the number of cells in each population by a factor."""
         if self.cell_graph is not None:
@@ -461,6 +465,7 @@ if __name__ == '__main__':
     net = TraubNet()
     net._generate_celltype_graph()
     net._generate_cell_graph()
-
+    net.create_network()
+    
 # 
 # trbnet.py ends here
