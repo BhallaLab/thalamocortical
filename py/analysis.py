66d1fd35a9cc3a9d4f9461612acaac1c38c0af15
Subhasis Ray <ray.subhasis@gmail.com>
2010-12-09 15:48:17 +0530

    Added a skeleton file for analysis code.

---
+++ py/analysis.py
@@ -0,0 +1,40 @@
+# analysis.py --- 
+# 
+# Filename: analysis.py
+# Description: 
+# Author: Subhasis Ray
+# Maintainer: 
+# Copyright (C) 2010 Subhasis Ray, all rights reserved.
+# Created: Thu Dec  9 15:45:26 2010 (+0530)
+# Version: 
+# Last-Updated: Thu Dec  9 15:47:45 2010 (+0530)
+#           By: Subhasis Ray
+#     Update #: 3
+# URL: 
+# Keywords: 
+# Compatibility: 
+# 
+# 
+
+# Commentary: 
+# 
+# Code for analyzing the data. This will perhaps be a module with all
+# the functions I develop. Currently three lines of analysis I am
+# thinking of are: 1. SEM analysis (Upi) 2. Granger causality (Ding)
+# 3. Ising model (Hertz) I am not sure I have enough time for the
+# latter two. But depends on how much effort I need to implement and
+# modify them.
+# 
+
+# Change log:
+# 
+# 
+# 
+
+# Code:
+
+
+
+
+# 
+# analysis.py ends here
