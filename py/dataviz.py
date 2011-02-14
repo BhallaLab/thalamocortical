462e98fa71efd7665337d4528c60ede178813fb6
Subhasis Ray <ray.subhasis@gmail.com>
2011-02-14 16:56:38 +0530

    Intermediate commit.

--- py/dataviz.py
+++ py/dataviz.py
@@ -7,9 +7,9 @@
 # Copyright (C) 2010 Subhasis Ray, all rights reserved.
 # Created: Wed Dec 15 10:16:41 2010 (+0530)
 # Version: 
-# Last-Updated: Thu Feb 10 22:23:11 2011 (+0530)
+# Last-Updated: Mon Feb 14 16:52:38 2011 (+0530)
 #           By: Subhasis Ray
-#     Update #: 407
+#     Update #: 636
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -38,6 +38,11 @@
 # consistent with the assumptions about table names between the
 # simulation code and the data analysis code.  Hence forking this away
 # into code for analyzing newer data.
+#
+# 2011-02-11 15:26:02 (+0530) -- scrapped matplotlib/mayavi approach
+# and going for simple 2D rasters with option for selecting tables and
+# scrolling (using Qt).
+#
 
 # Code:
 
@@ -50,6 +55,8 @@ import numpy as np
 from pysparse.sparse.spmatrix import ll_mat
 import tables
 from PyQt4 import QtCore, QtGui
+from PyQt4 import Qwt5 as Qwt
+
 
 ITERS = 1000
 
@@ -103,6 +110,29 @@ class TraubData:
         except tables.NoSuchNodeError:
             print 'No node called /Ca'
 
+            
+    def get_data_by_name(self, names, datatype):
+        """Return the data tables listed in names.
+
+        If datatype is None then use the name to recognize type -
+        ending with ca will be a [Ca2+] table, ending with Vm will be
+        a Vm table, with spike will be a spike table.
+        
+        """
+        ret = None
+        target_node = None
+        if datatype.lowercase() == 'ca':
+            target_node = '/Ca'
+        elif datatype.lowercase() == 'vm':
+            target_node = '/Vm'
+        elif datatype.lowercase() == 'spike':
+            target_node = '/spikes'
+        else:
+            print 'Invalid data type: %s' % (datatype)
+            return None
+        ret = [self.h5f.getNode(target_node + '/' + name) for name in names]
+        return ret
+
     def get_data_by_re(self, pattern, datatype):
         """Returns data for cells whose name match regular expression 'pattern'"""
         ret = []
@@ -123,37 +153,179 @@ class TraubData:
         return ret
                 
     def __del__(self):
-        if self.h5f and self.h5f.isopen():
+        # I think this is somewhat redundant - pytables takes care of
+        # closing remaining open files.
+        print 'del called'
+        if self.h5f and self.h5f.isopen:
             self.h5f.close()
+            print 'closed file'
 
+
+        
 
 class GuiModel(QtCore.QObject):
     """data_handler -- TraubData object accessing the files.
 
-    ca_cells -- 
+    ca_cells -- list of cells whose [Ca2+] data is available.
+
+    vm_cells -- list of cells whose Vm data is available.
+
+    spike_cells -- list of cells whose spiketrains are available.
     """
     def __init__(self, *args):
         QtCore.QObject.__init__(self, *args)
         self.data_handler = None
-        self.ca_cells = []
-        self.vm_cells = []
-        self.spike_cells = []
+        self.selected_ca = QtGui.QStringListModel()
+        self.selected_vm = QtGui.QStringListModel()
+        self.selected_spikes = QtGui.QStringListModel()
+        self.available_ca = QtGui.QStringListModel()
+        self.available_vm = QtGui.QStringListModel()
+        self.available_spikes = QtGui.QStringListModel()
         
     def openFile(self, filename):
         """Open a new data file given by filename."""
         self.data_handler = TraubData(filename)
-        # Clear the lists of arrays to be plotted
-        self.ca_cells = []
-        self.vm_cells = []
-        self.spike_cells = []
+        self.available_spikes.setStringList(self.data_handler.spiketrain_names)
+        self.available_vm.setStringList(self.data_handler.Vm_names)
+        self.available_ca.setStringList(self.data_handler.Ca_names)
+        self.emit(QtCore.SIGNAL('file_loaded(string)'), filename)
         
+
+    # def addTables(self, table_list, table_type):
+    #     target = None
+    #     source = None
+    #     if table_type.lower() == 'ca':
+    #         target = self.selected_ca
+    #         source = self.available_ca
+    #     elif table_type.lower() == 'vm':
+    #         target = self.selected_vm
+    #         source = self.available_vm
+    #     elif table_type.lower() == 'spike':
+    #         target = self.selected_spikes
+    #         source = self.available_spikes
+    #     else:
+    #         raise Exception('No such table type: %s' % table_type)
         
+    #     for table in table_list:
+    #         source.remove(table)
+    #         target.append(table)
+    #     self.emit(QtCore.SIGNAL('tablelist_updated()'))
+        
+
+    # def removeTables(self, table_list, table_type):
+    #     target = None
+    #     source = None
+    #     if table_type.lower() == 'ca':
+    #         source = self.selected_ca
+    #         target = self.available_ca
+    #     elif table_type.lower() == 'vm':
+    #         source = self.selected_vm
+    #         target = self.available_vm
+    #     elif table_type.lower() == 'spike':
+    #         source = self.selected_spikes
+    #         target = self.available_spikes
+    #     else:
+    #         raise Exception('No such table type: %s' % table_type)
+        
+    #     for table in table_list:
+    #         source.remove(table)
+    #         target.append(table)
+    #         target.sort()
+    #     self.emit(QtCore.SIGNAL('tablelist_updated()'))
+        
+
 class DataVizGui(QtGui.QMainWindow):
     def __init__(self, *args):
         QtGui.QMainWindow.__init__(self, *args)
+        self.setDockOptions(self.AllowNestedDocks | self.AllowTabbedDocks | self.ForceTabbedDocks | self.AnimatedDocks)
+        self.setDockNestingEnabled(True)
+        self.gui_model = GuiModel()
+        # self.make_table_lists()
+        self.plot = Qwt.QwtPlot()
+        self.setCentralWidget(self.plot)
+        self.make_actions()
+        self.make_menu()
+
         
+    def make_actions(self):
+        self.fopen_action = QtGui.QAction(self.tr('&Open'), self)
+        self.connect(self.fopen_action, QtCore.SIGNAL('triggered()'), self.popup_fopen)
+        self.quit_action = QtGui.QAction(self.tr('&Quit'), self)
+        self.connect(self.quit_action, QtCore.SIGNAL('triggered()'), self.do_quit)
+        self.show_available_tables_action = QtGui.QAction(self.tr('Show &Available Tables'), self)
+        self.show_selected_tables_action = QtGui.QAction(self.tr('Show &Selected Tables'), self)             
+        self.show_available_ca_action = QtGui.QAction(self.tr('Show available Ca tables'), self)
+        self.show_available_vm_action = QtGui.QAction('Show available Vm tables', self)
+        self.show_available_spike_action = QtGui.QAction('Show available spike tables', self)
+        self.show_selected_ca_action = QtGui.QAction('Show selected Ca tables', self)
+        self.show_selected_vm_action = QtGui.QAction('Show selected Vm tables', self)
+        self.show_selected_spike_action = QtGui.QAction('Show selected spike tables', self)
+        
+        
+    def make_menu(self):
+        self.file_menu = QtGui.QMenu('&File', self)
+        self.file_menu.addAction(self.fopen_action)
+        self.file_menu.addAction(self.quit_action)
 
+        self.view_menu = QtGui.QMenu('&View', self)
+        self.view_menu.addAction(self.show_available_tables_action)
+        self.view_menu.addAction(self.show_selected_tables_action)
+        self.view_menu.addAction(self.show_available_spike_action)
+        self.view_menu.addAction(self.show_available_vm_action)
+        self.view_menu.addAction(self.show_available_ca_action)
+        self.view_menu.addAction(self.show_selected_spike_action)
+        self.view_menu.addAction(self.show_selected_vm_action)
+        self.view_menu.addAction(self.show_selected_ca_action)
+        self.menuBar().addMenu(self.file_menu)
+        self.menuBar().addMenu(self.view_menu)
+        
+    def make_table_lists(self):
+        self.selected_ca_view = QtGui.QListView(self)
+        self.selected_vm_view = QtGui.QListView(self)
+        self.selected_spikes_view = QtGui.QListView(self)
+        self.available_ca_view = QtGui.QListView(self)
+        self.available_vm_view = QtGui.QListView(self)
+        self.available_spikes_view = QtGui.QListView(self)
 
+        self.selected_ca_view.setObjectName('selected_ca')
+        self.selected_vm_view.setObjectName('selected_vm')
+        self.selected_spikes_view.setObjectName('selected_spikes')
+        self.available_ca_view.setObjectName('available_ca')
+        self.available_vm_view.setObjectName('available_vm')
+        self.available_spikes_view.setObjectName('available_spikes')
+
+        self.selected_ca_view.setModel(self.gui_model.selected_ca)
+        self.selected_vm_view.setModel(self.gui_model.selected_vm)
+        self.selected_spikes_view.setModel(self.gui_model.selected_spikes)
+        self.available_ca_view.setModel(self.gui_model.available_ca)
+        self.available_vm_view.setModel(self.gui_model.available_vm)
+        self.available_spikes_view.setModel(self.gui_model.available_spikes)
+        self.tables_docks = []
+        self.list_views = [self.selected_ca_view, self.selected_vm_view,
+                 self.selected_spikes_view, self.available_ca_view,
+                 self.available_vm_view, self.available_spikes_view]
+        for view in self.list_views:
+            view.setAcceptDrops(True)
+            view.setDragEnabled(True)
+            view.setDropIndicatorShown(True)
+            view.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
+            self.tables_docks.append(QtGui.QDockWidget(view.objectName(), self))
+            self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.tables_docks[-1])
+        
+    def popup_fopen(self):
+        file_dialog = QtGui.QFileDialog(self)
+        file_dialog.setFileMode(QtGui.QFileDialog.ExistingFile)
+        file_dialog.setFilter(self.tr('hdf5(*.h5 *.hd5 *.hdf5);;All files(*)'))
+        if file_dialog.exec_():
+            filenames = file_dialog.selectedFiles()
+            self.gui_model.openFile(str(filenames[0]))
+
+    def do_quit(self):
+        print 'Quitting'
+        if self.gui_model.data_handler:
+            del self.gui_model.data_handler
+            print 'Deleted gui model object'
+        QtGui.qApp.quit()
 
             
 class __TraubData:
@@ -267,20 +439,11 @@ class __TraubData:
         self.draw()
 
 
-color_list = ['b', 'r', 'g', 'm', 'y', 'c', 'k']
-color_convert = lambda x: colorConverter.to_rgb(x)
-if __name__ == '__main__':    
-    data = []
-    for i in range(14):
-        data.append(np.random.rand(2, 1000))
-    fig = pylab.figure()
-    ax = p3.Axes3D(fig)
-    for ii in range(14):
-        color = color_convert(color_list[ii%len(color_list)])
-        print color
-        ax.scatter3D(data[i][0], data[i][1], ii/14.0, c=color)
-    # pylab.colorbar()
-    pylab.show()
-    
+if __name__ == '__main__':
+    app = QtGui.QApplication([])
+    mainwin = DataVizGui()
+    app.lastWindowClosed.connect(mainwin.do_quit)
+    mainwin.show()
+    app.exec_()
 # 
 # dataviz.py ends here
