0866b9859338bf9d26e2334323bac152d03e393d
Subhasis <ray.subhasis@gmail.com>
2011-07-20 10:40:17 +0530

    Added the command line for running simulation with most commonly ued settings.

---
+++ py/commandline.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#nohup python trbsim.py -t 1.0 --reseed | gzip -c - > nohup_`date '+%Y%m%d_%H%M%S'`.gz 
+#python trbsim.py -t 1.0 --reseed 2>&1 | gzip -c - > nohup_`date '+%Y%m%d_%H%M%S'`.gz
+pushd /data/subha/cortical/py
+python26 trbsim.py -t 5.0 -x 0.8 --reseed --stochastic > nohup_`date '+%Y%m%d_%H%M%S'` 2>&1
+popd
