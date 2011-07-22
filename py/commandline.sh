f93d53d254b1ac423d353eaddd75a74bd217f82e
Subhasis <ray.subhasis@gmail.com>
2011-07-22 16:20:31 +0530

    Updated command line script to run every 36 hours using at.

--- py/commandline.sh
+++ py/commandline.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #nohup python trbsim.py -t 1.0 --reseed | gzip -c - > nohup_`date '+%Y%m%d_%H%M%S'`.gz 
 #python trbsim.py -t 1.0 --reseed 2>&1 | gzip -c - > nohup_`date '+%Y%m%d_%H%M%S'`.gz
-pushd /data/subha/cortical/py
+at now + 36 hours -f /data/subha/cortical/py/commandline.sh
 python26 trbsim.py -t 5.0 -x 0.8 --reseed --stochastic > nohup_`date '+%Y%m%d_%H%M%S'` 2>&1
-popd
