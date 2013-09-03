f53e2e1a3f94c7287998ecca9ca16dd3ac0c86de
subhacom <ray.subhasis@gmail.com>
2013-09-03 14:56:53 +0530

    Added the batch run script. Fixed recording from synapses - earlier NMDA was being skipped. wildcard with ISA=SynChan was not capturing NMDAChan. 'for synapse in synchan + nmdachain' was also skipping the nmdachan. Now have debug prints to make sure that these are added. Also recording all synaptic conductances on cell#0 of each celltype

---
+++ py/batchrun.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+count=10
+if (($# > 0)); then
+    count=$1
+fi
+dbcount=55
+if (($# > 1)); then
+    dbcount=$2
+fi
+for ((ii=1; ii <= $count; ++ii)); do
+    echo "$ii"
+    nohup python trbsim.py -n "Running #$ii-th simulation of $dbcount deep basket cells" &> nohup_`date '+%Y%m%d_%H%M%S'`.log &
+    wait ${!}
+    echo "Done running #$ii-th simulation with $dbcount deep basket cells"
+done
+
+
