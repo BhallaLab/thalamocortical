4232ecb67348bf3309d93ca32942d023414e18b0
subhacom <ray.subhasis@gmail.com>
2013-09-26 17:55:45 +0530

    Updated batch run comments to be passed to traubsim.

--- py/batchrun.sh
+++ py/batchrun.sh
@@ -9,7 +9,7 @@ if (($# > 1)); then
 fi
 for ((ii=1; ii <= $count; ++ii)); do
     echo "$ii"
-    nohup python trbsim.py -n "Running #$ii-th simulation of $dbcount deep basket cells" &> nohup_`date '+%Y%m%d_%H%M%S'`.log &
+    nohup python trbsim.py -n "Running #$ii-th simulation of $dbcount deep basket cells with normal distribution of synaptic conductances but no variability in individual cells." &> nohup_`date '+%Y%m%d_%H%M%S'`.log &
     wait ${!}
     echo "Done running #$ii-th simulation with $dbcount deep basket cells"
 done
