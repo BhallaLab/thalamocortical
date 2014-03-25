2360ee3597f12c7eeb46b1e5edaa6c4e59f042b8
subhacom <ray.subhasis@gmail.com>
2014-03-25 15:17:30 +0530

    Updated trbnet to replace negative Gbar with mean of Gaussian. custom.ini updated to do lognorm with 30 db and add 10% sd to active conductances.  Updated batchrun to run lognorm for 30 basket cells.

--- py/batchrun.sh
+++ py/batchrun.sh
@@ -3,13 +3,13 @@ count=10
 if (($# > 0)); then
     count=$1
 fi
-dbcount=55
+dbcount=30
 if (($# > 1)); then
     dbcount=$2
 fi
 for ((ii=1; ii <= $count; ++ii)); do
     echo "$ii"
-    nohup python trbsim.py -n "Running #$ii-th simulation of $dbcount deep basket cells with normal distribution of synaptic conductances but no variability in individual cells." &> nohup_`date '+%Y%m%d_%H%M%S'`.log &
+    nohup python trbsim.py -n "Running #$ii-th simulation of $dbcount deep basket cells with lognormal distribution of synaptic conductances." &> nohup_`date '+%Y%m%d_%H%M%S'`.log &
     wait ${!}
     echo "Done running #$ii-th simulation with $dbcount deep basket cells"
 done
