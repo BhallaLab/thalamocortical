2d230f647bb97b0bdfde829dce49383ca6ebaf86
subhacom <ray.subhasis@gmail.com>
2014-04-23 10:07:58 +0530

    Added ectopicinterval attribute.

--- py/create_unconnected_celltype_graph.py
+++ py/create_unconnected_celltype_graph.py
@@ -5,4 +5,5 @@ g = ig.Graph(0)
 g.add_vertices(len(tndata.celltype))
 g.vs['label'] = tndata.celltype
 g.vs['count'] = tndata.cellcount
+g.vs['ectopicinterval'] = tndata.ectopic_interval
 g.write_graphml('unconnected_celltype_net.graphml')
