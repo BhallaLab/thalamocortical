57a897de7b0147c262b295d42f19fa9cf47ca4bb
subhacom <ray.subhasis@gmail.com>
2014-07-04 09:30:42 +0530

    Updated notes.
    Corrected simulation.py to put the
    right simtime in file attribute (though it is
    always available under /runconfig in data file).
    
    updated batchrun to take the distribution name
    as well.
    
    updated custom.ini to run 40 deepbasket with
    normal distribution.

--- py/batchrun.sh
+++ py/batchrun.sh
@@ -7,9 +7,14 @@ dbcount=40
 if (($# > 1)); then
     dbcount=$2
 fi
+distr=unknown
+if (($# > 2)); then
+    distr=$3
+fi
+
 for ((ii=1; ii <= $count; ++ii)); do
     echo "$ii"
-    nohup python trbsim.py -n "Running #$ii-th simulation of $dbcount deep basket cells with normal distribution of synaptic conductances." &> nohup_`date '+%Y%m%d_%H%M%S'`.log &
+    nohup python trbsim.py -n "Running #$ii-th simulation of $dbcount deep basket cells with $distr distribution of synaptic conductances." &> nohup_`date '+%Y%m%d_%H%M%S'`.log &
     wait ${!}
     echo "Done running #$ii-th simulation with $dbcount deep basket cells"
 done
