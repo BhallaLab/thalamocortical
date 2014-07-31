f8cc7ee9ad9fd6e78d00484c7d87cebfe7ee0822
subhacom <ray.subhasis@gmail.com>
2014-07-31 15:39:45 +0530

    Updated custom.ini to run 60 deep basket

--- py/mpirun_command.sh
+++ py/mpirun_command.sh
@@ -1,2 +1,2 @@
-mpirun -x LD_LIBRARY_PATH -x PYTHONPATH=$PYTHONPATH:/home/subhasis/moose_trunk_20131016/python -hostfile hostfile /usr/local/bin/python2.7 trbsim.py -n "Running on 30db normal `hostname`"
+mpirun -x LD_LIBRARY_PATH -x PYTHONPATH=$PYTHONPATH:/home/subhasis/moose_trunk_20131016/python -hostfile hostfile /usr/local/bin/python2.7 trbsim.py -n "Running on 60db normal `hostname`"
 
